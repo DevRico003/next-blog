ba7f64ea8739a853d3ee15c3c1a828b827835b22
Enrico Carteciano <carteciano.enrico@gmail.com>
2023-10-09 19:52:25 +0200

    deleted s3

--- src/app/api/create/route.ts
+++ src/app/api/create/route.ts
@@ -1,23 +1,27 @@
-// Importiere die benötigten Module
+// Importieren der erforderlichen Module
 import { NextRequest, NextResponse } from "next/server"
-import AWS from 'aws-sdk'
+import fs from 'fs'
+import path from 'path'
 
-// Konfiguriere die AWS-Region
-AWS.config.update({ region: 'eu-central-1' })
-
-// Erstelle eine neue S3-Instanz
-const s3 = new AWS.S3()
-
-// Definiere die API-Route für POST-Anfragen
+// Definieren der POST-Methode für die API-Route
 export const POST = async (request: NextRequest, response: NextResponse) => {
   try {
-    // Parse den Request-Body als JSON
+    // Parsen des Anfragekörpers in ein JSON-Objekt
     const body = await request.json()
 
-    // Zerlege den Body in seine Bestandteile
+    // Extrahieren der relevanten Felder aus dem Anfragekörper
     const { title, subtitle, author, date, content } = body
 
-    // Erstelle den Markdown-Inhalt für den Post
+    // Erstellen eines neuen Post-Objekts
+    const post = {
+      title,
+      subtitle,
+      author,
+      date,
+      content,
+    }
+
+    // Erstellen des Markdown-Inhalts für den neuen Post
     const mdContent = `---
 title: "${title}"
 subtitle: "${subtitle}"
@@ -27,29 +31,19 @@ date: "${date}"
 ${content}
 `
 
-    // Definiere die Parameter für den S3-PutObject-Aufruf
-    const params = {
-      Bucket: 'next-blog-devrico003', // Dein S3-Bucket-Name
-      Key: `${title.replace(/ /g, '_')}.md`, // Der Dateiname im S3-Bucket
-      Body: mdContent,
-      ContentType: 'text/markdown'
-    }
+    // Definieren des Pfads für die neue .md-Datei
+    // Der Titel wird so formatiert, dass Leerzeichen durch Unterstriche ersetzt werden
+    const filePath = path.join(process.cwd(), 'src', 'posts', `${title.replace(/ /g, '_')}.md`)
 
-    // Speichere die Datei im S3-Bucket
-    s3.putObject(params, function(err, data) {
-      if (err) {
-        // Logge den Fehler und sende eine Fehlerantwort
-        console.log(err)
-        return NextResponse.error()
-      } else {
-        // Sende eine Erfolgsantwort mit dem erstellten Post
-        return NextResponse.json({ success: true, post: body }, { status: 200 })
-      }
-    })
+    // Schreiben des Markdown-Inhalts in eine neue .md-Datei
+    fs.writeFileSync(filePath, mdContent)
 
+    // Senden einer Erfolgsmeldung als Antwort
+    return NextResponse.json({ success: true, post: post }, { status: 200 })
   } catch (error) {
-    // Logge den Fehler und sende eine Fehlerantwort
+    // Protokollieren des Fehlers, falls einer auftritt
     console.log(error)
+    // Senden einer Fehlermeldung als Antwort
     return NextResponse.error()
   }
 }
