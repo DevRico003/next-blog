520f0deac0d5fc5fd6200acd1a75f76a577442d3
Enrico Carteciano <carteciano.enrico@gmail.com>
2023-10-09 19:40:24 +0200

    added s3 bucket

--- src/app/api/create/route.ts
+++ src/app/api/create/route.ts
@@ -1,27 +1,23 @@
-// Importieren der erforderlichen Module
+// Importiere die benötigten Module
 import { NextRequest, NextResponse } from "next/server"
-import fs from 'fs'
-import path from 'path'
+import AWS from 'aws-sdk'
 
-// Definieren der POST-Methode für die API-Route
+// Konfiguriere die AWS-Region
+AWS.config.update({ region: 'eu-central-1' })
+
+// Erstelle eine neue S3-Instanz
+const s3 = new AWS.S3()
+
+// Definiere die API-Route für POST-Anfragen
 export const POST = async (request: NextRequest, response: NextResponse) => {
   try {
-    // Parsen des Anfragekörpers in ein JSON-Objekt
+    // Parse den Request-Body als JSON
     const body = await request.json()
 
-    // Extrahieren der relevanten Felder aus dem Anfragekörper
+    // Zerlege den Body in seine Bestandteile
     const { title, subtitle, author, date, content } = body
 
-    // Erstellen eines neuen Post-Objekts
-    const post = {
-      title,
-      subtitle,
-      author,
-      date,
-      content,
-    }
-
-    // Erstellen des Markdown-Inhalts für den neuen Post
+    // Erstelle den Markdown-Inhalt für den Post
     const mdContent = `---
 title: "${title}"
 subtitle: "${subtitle}"
@@ -31,19 +27,29 @@ date: "${date}"
 ${content}
 `
 
-    // Definieren des Pfads für die neue .md-Datei
-    // Der Titel wird so formatiert, dass Leerzeichen durch Unterstriche ersetzt werden
-    const filePath = path.join(process.cwd(), 'src', 'posts', `${title.replace(/ /g, '_')}.md`)
+    // Definiere die Parameter für den S3-PutObject-Aufruf
+    const params = {
+      Bucket: 'next-blog-devrico003', // Dein S3-Bucket-Name
+      Key: `${title.replace(/ /g, '_')}.md`, // Der Dateiname im S3-Bucket
+      Body: mdContent,
+      ContentType: 'text/markdown'
+    }
 
-    // Schreiben des Markdown-Inhalts in eine neue .md-Datei
-    fs.writeFileSync(filePath, mdContent)
+    // Speichere die Datei im S3-Bucket
+    s3.putObject(params, function(err, data) {
+      if (err) {
+        // Logge den Fehler und sende eine Fehlerantwort
+        console.log(err)
+        return NextResponse.error()
+      } else {
+        // Sende eine Erfolgsantwort mit dem erstellten Post
+        return NextResponse.json({ success: true, post: body }, { status: 200 })
+      }
+    })
 
-    // Senden einer Erfolgsmeldung als Antwort
-    return NextResponse.json({ success: true, post: post }, { status: 200 })
   } catch (error) {
-    // Protokollieren des Fehlers, falls einer auftritt
+    // Logge den Fehler und sende eine Fehlerantwort
     console.log(error)
-    // Senden einer Fehlermeldung als Antwort
     return NextResponse.error()
   }
 }
